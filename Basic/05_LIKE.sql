c5133f005e10aa548a7e06d9e2ae1aaf7b231e48
JasonW99 <ningweiw@gmail.com>
2017-07-28 15:40:08 -0700

    select

--- Basic/05_LIKE.sql
+++ Basic/05_LIKE.sql
@@ -4,11 +4,20 @@ SELECT *
 FROM testDB
 WHERE "group" LIKE 'Snoop%'
 /*
-Note: "group" appears in quotations above because GROUP is actually the name of a function in SQL. The double quotes (as opposed to single: ') are a way of indicating that you are referring to the column name "group", not the SQL function. In general, putting double quotes around a word or phrase will indicate that you are referring to that column name.
-The % used above represents any character or set of characters. In the type of SQL that Mode uses, LIKE is case-sensitive, meaning that the above query will only capture matches that start with a capital “S” and lower-case “noop.” 
+Note: 
+"group" appears in quotations above because GROUP is actually the name of a function in SQL.
+The double quotes (as opposed to single: ') are a way of indicating that you are referring to the column name "group", not the SQL function. 
+In general, putting double quotes around a word or phrase will indicate that you are referring to that column name.
+
+
+The % used above represents any character or set of characters. 
+
+In the type of SQL that Mode uses, LIKE is case-sensitive. 
+for example, the above query will only capture matches that start with a capital “S” and lower-case “noop.” 
+snoop and Snoop will be differnt.
+However if we want to ignore case when matching values, then we can use ILIKE command as following:
 */
 
---To ignore case when you’re matching values, you can use the ILIKE command as following:
 SELECT *
 FROM testDB
 WHERE "group" ILIKE 'snoop%'
